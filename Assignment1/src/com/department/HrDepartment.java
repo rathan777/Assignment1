d91628445ae20a9b26b63b89da2e9625ca18e2ee
rathan777 <106870266+rathan777@users.noreply.github.com>
2022-06-29 11:06:23 +0530

    Graded coding assignment
    Graded coding assignment

---
+++ Assignment1/src/com/department/HrDepartment.java
@@ -0,0 +1,26 @@
+package com.department;
+
+public class HrDepartment extends SuperDepartment {
+
+	//public String doActivity()
+	{
+		//return "team lunch";
+	}
+	public String departmentName()
+	{
+		return "Hr Department";
+	}
+	public String getTodaysWork()
+	{
+	return "Fill todays worksheet and mark your attendance"	;
+	
+	}
+	public String getWorkDeadline()
+	{
+		return "Complete by EOD";
+	}
+	public String doActivity()
+	{
+		return "team lunch";
+	}
+}
---
+++ Assignment1/src/com/department/SuperDepartment.java
@@ -0,0 +1,30 @@
+package com.department;
+
+public class SuperDepartment {
+	
+     String departmentName,getTodaysWork,getWorkDeadline,isTodayaHoliday;
+     
+	public String departmentName()
+	{
+		String S = "Super Department";
+			return S;
+	}
+	public String getTodaysWork()
+	{
+		String S2= "No work as of now";
+		return S2;
+	}
+	public String getWorkDeadline()
+	{
+		String S3 = "Nil";
+		return S3;
+	}
+	public String isTodayaHoliday()
+	{
+		String S4 = "Today is not a holiday";
+		return S4;
+	}
+		 	
+	}
+
+
